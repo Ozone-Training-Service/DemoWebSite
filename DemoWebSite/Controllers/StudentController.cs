3aaec30c06cbb28e5f7b32b9b13a1212fd6e0559
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-20 12:00:26 +0530

    Add project files.

---
+++ DemoWebSite/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebSite.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        // Action method
+        public IActionResult Index()
+        {
+
+            string name = "Vsihnu";
+        
+           
+            return View();
+        }
+
+        public IActionResult AddStudent()  //        Student/AddStudent
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            
+
+            ViewBag.Name= "Vishnu Kumar";
+            ViewBag.Age = 16;
+            ViewBag.Address = "Kollam";
+
+
+            ViewData["FirstName"] = "Vishnu";
+            ViewData["LastName"] = "Kumar";
+
+            return View();
+        }
+
+
+        public string Hello()
+        {
+            return "Hello";
+        }
+
+
+    }
+}
---
+++ DemoWebSite/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebSite.Controllers
+{
+    public class TeacherController:Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddTeacher(string name,string dpt)
+        {
+
+            ViewBag.Name = name;
+            ViewBag.Dpt = dpt;
+
+            return View();
+        }
+    }
+}
