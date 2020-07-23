7848d1349830f2b2ea6f56716898c12aeabcf906
vishnukumarps <vishnukumar29518@gmail.com>
2020-07-23 10:06:08 +0530

    Model

---
+++ DemoWebSite/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebSite.Models
+{
+    public class Student
+    {
+
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public string Dob { get; set; }
+        public int RollNo { get; set; }
+
+    }
+}
---
+++ DemoWebSite/Models/Teacher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebSite.Models
+{
+    public class Teacher
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string MobileNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
